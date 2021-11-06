ca1fc6ecd710f48ebf03f8ec62d9cdba61b881e8
Peter Hui <huishikuen@gmail.com>
2021-11-06 13:42:59 -0700

    added loading spinner

--- public/index.js
+++ public/index.js
@@ -1,27 +1,62 @@
-const tryButton = document.getElementById("try-video-button");
-const stopButton = document.getElementById("stop-video-button");
-const container = document.getElementById("video-div");
+// More API functions here:
+// https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
 
-async function startVideo() {
-  startVideoUI();
-  const track = await Twilio.Video.createLocalVideoTrack();
-  container.append(track.attach());
-  stopButton.addEventListener("click", function () {
-    stopVideo(track);
-  });
+// the link to your model provided by Teachable Machine export panel
+const URL = 'https://teachablemachine.withgoogle.com/models/a-J-UjsEB/';
+
+let model, webcam, labelContainer, maxPredictions;
+
+
+// Load the image model and setup the webcam
+async function init() {
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
+
+    // load the model and metadata
+    // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+    // or files from your local hard drive
+    // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
+
+    // Convenience function to setup a webcam
+    const flip = true; // whether to flip the webcam
+    webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
+    await webcam.setup(); // request access to the webcam
+    await webcam.play();
+    window.requestAnimationFrame(loop);
+
+    // append elements to the DOM
+    document.getElementById("webcam-container").appendChild(webcam.canvas);
+    labelContainer = document.getElementById("label-container");
+    for (let i = 0; i < maxPredictions; i++) { // and class labels
+        labelContainer.appendChild(document.createElement("div"));
+    }
+    return true;
 }
-function stopVideo(track) {
-  track.stop();
-  endVideoUI();
+
+async function loop() {
+    webcam.update(); // update the webcam frame
+    await predict();
+    window.requestAnimationFrame(loop);
 }
-function startVideoUI() {
-  tryButton.disabled = true;
-  stopButton.disabled = false;
-  container.innerHTML = "";
+
+// run the webcam image through the image model
+async function predict() {
+    // predict can take in an image, video or canvas html element
+    const prediction = await model.predict(webcam.canvas);
+    for (let i = 0; i < maxPredictions; i++) {
+        const classPrediction =
+            prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+        labelContainer.childNodes[i].innerHTML = classPrediction;
+    }
 }
-function endVideoUI() {
-  tryButton.disabled = false;
-  stopButton.disabled = true;
-  container.innerHTML = "";
-}
-tryButton.addEventListener("click", startVideo);
+
+
+
+(async() => {
+  const initialized = await init();
+  if (initialized){
+    document.querySelector('#loadingSpinner').style.display = "none";
+  }
+})();
